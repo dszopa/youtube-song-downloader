f698b7b9fea9dc396a92843c326cd9330f5ad507
dszopa <szopanator@gmail.com>
2016-02-19 19:34:33 -0600

    Add UTests for exactDownloadSongByQueryAndDuration & largeBufferDownloadSongByQueryAndDuration

--- tests/test_songDownloader.py
+++ tests/test_songDownloader.py
@@ -10,20 +10,20 @@ songDownloader = ytsdl.songDownloader()
 def test_directoryIsCorrect():
 	assert os.getcwd() == "/Users/danny/Documents/Coding/Python/youtube-song-downloader/bin"
 
-# def test_printUsage():
-# 	assert 1 == 1
+def test_printUsage():
+	assert 1 == 1
 
-# def test_verifyNoDuplicateSong():
-# 	assert 1 == 1
+def test_verifyNoDuplicateSong():
+	assert 1 == 1
 
-# def test_verifySearchResults():
-# 	assert 1 == 1
+def test_verifySearchResults():
+	assert 1 == 1
 
-# def test_editDownloadLocation():
-# 	assert 1 == 1
+def test_editDownloadLocation():
+	assert 1 == 1
 
-# def test_usePrevDirectory():
-# 	assert 1 == 1
+def test_usePrevDirectory():
+	assert 1 == 1
 
 def test_downloadSongByQuery():
 	songDownloader.downloadSongByQuery("Rick Astley - Never Gonna Give You Up")
@@ -38,6 +38,28 @@ def test_downloadSongByQuery():
 
 def test_downloadSongByQueryAndDuration():
 	songDownloader.downloadSongByQueryAndDuration("Rick Astley - Never Gonna Give You Up", "3:33", 5)
+	filenameList = os.listdir(os.getcwd())
+	containsFile = False
+	if "Rick Astley - Never Gonna Give You Up.mp3" in filenameList:
+		containsFile = True
+	assert containsFile == True
+	os.chdir("/Users/danny/Music/test/")
+	os.system("rm -f \"Rick Astley - Never Gonna Give You Up.mp3\"")
+	os.chdir("/Users/danny/Documents/Coding/Python/youtube-song-downloader/bin")
+
+def test_exactDownloadSongByQueryAndDuration():
+	songDownloader.downloadSongByQueryAndDuration("Rick Astley - Never Gonna Give You Up", "3:33", 0)
+	filenameList = os.listdir(os.getcwd())
+	containsFile = False
+	if "Rick Astley - Never Gonna Give You Up.mp3" in filenameList:
+		containsFile = True
+	assert containsFile == True
+	os.chdir("/Users/danny/Music/test/")
+	os.system("rm -f \"Rick Astley - Never Gonna Give You Up.mp3\"")
+	os.chdir("/Users/danny/Documents/Coding/Python/youtube-song-downloader/bin")
+
+def test_largeBufferDownloadSongByQueryAndDuration():
+	songDownloader.downloadSongByQueryAndDuration("Rick Astley - Never Gonna Give You Up", "3:33", 30)
 	filenameList = os.listdir(os.getcwd())
 	containsFile = False
 	if "Rick Astley - Never Gonna Give You Up.mp3" in filenameList:
