a0cb6af6581f2838737511fa268bf45d04783ba9
dszopa <szopanator@gmail.com>
2016-02-22 14:48:55 -0600

    Add setup.py implementation, MANIFEST.in  & Rename youtube-song-downloader

--- setup.py
+++ setup.py
@@ -1,8 +1,21 @@
 #!/usr/bin/env python
 
-import os
+from setuptools import setup
 
 # Make sure that python 2.7 and FFMPEG are installed
-os.system("pip install youtube_dl")
-os.system("pip install --upgrade google-api-python-client")
-os.system("pip install pytest")
+setup(name='youtube-song-downloader',
+      version='0.1',
+      description='A program used to search and download songs from Youtube.',
+      url='https://github.com/dszopa/youtube-song-downloader',
+      author='Daniel Szopa',
+      author_email='dszopa@iastate.edu',
+      license='MIT',
+      install_requires=[
+          'youtube_dl',
+          'google-api-python-client',
+          'pytest',
+      ],
+      packages=['ytsdl'],
+      scripts=['bin/youtube-song-downloader'],
+      include_package_data=True,
+      zip_safe=False)
